6f8cb281c4f620fcda3da7251a210e923f1fab85
Marko Caric <pbmc117@gmail.com>
2021-08-15 21:23:01 -0700

    added service worker

--- public/db.js
+++ public/db.js
@@ -1,16 +1,111 @@
-// TODO: setup indexedDb connection and create an object store for saving
-// transaction data when the user is offline.
+// // TODO: setup indexedDb connection and create an object store for saving
+// // transaction data when the user is offline.
 
-function saveRecord(record) {
-  // TODO: this function should save a transaction object to indexedDB so that
-  // it can be synced with the database when the user goes back online.
-}
+// function saveRecord(record) {
+//   // TODO: this function should save a transaction object to indexedDB so that
+//   // it can be synced with the database when the user goes back online.
+// }
+
+// function checkDatabase() {
+//   // TODO: this function should check for any saved transactions and post them
+//   // all to the database. Delete the transactions from IndexedDB if the post
+//   // request is successful.
+// }
+
+// // listen for app coming back online
+// window.addEventListener('online', checkDatabase);
+
+const budgetVersion = 1;
+let db;
+
+// Create a new db request for a "budget" database.
+const request = indexedDB.open("BudgetDB", budgetVersion);
+
+request.onerror = function (e) {
+  console.log(e.target);
+  console.log(`Woops! ${e.target.errorCode}`);
+};
+
+request.onupgradeneeded = function (e) {
+  console.log("Upgrade needed in IndexDB");
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore("BudgetStore", { autoIncrement: true });
+  }
+};
+
+request.onsuccess = function (e) {
+  console.log("success");
+  db = e.target.result;
+
+  // Check if app is online before reading from db
+  if (navigator.onLine) {
+    console.log("Backend online! 🗄️");
+    checkDatabase();
+  }
+};
 
 function checkDatabase() {
-  // TODO: this function should check for any saved transactions and post them
-  // all to the database. Delete the transactions from IndexedDB if the post
-  // request is successful.
+  console.log("check db invoked");
+
+  // Open a transaction on your BudgetStore db
+  const transaction = db.transaction(["BudgetStore"], "readonly");
+
+  // access your BudgetStore object
+  const store = transaction.objectStore("BudgetStore");
+
+  // Get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  // If the request was successful
+  getAll.onsuccess = async function () {
+    if (getAll.result.length === 0) {
+      // no items to post to backend
+      return;
+    }
+    // If there are items in the store, we need to bulk add them when we are back online
+    const response = await fetch("/api/transaction/bulk", {
+      method: "POST",
+      body: JSON.stringify(getAll.result),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    });
+    const dbTransactions = await response.json();
+    // If our returned response is not empty
+    if (dbTransactions.length > 0) {
+      // Open another transaction to BudgetStore with the ability to read and write
+      const delTxn = db.transaction(["BudgetStore"], "readwrite");
+
+      // Assign the current store to a variable
+      const currentStore = delTxn.objectStore("BudgetStore");
+
+      // Clear existing entries because our bulk add was successful
+      currentStore.clear();
+      console.log("Clearing store 🧹");
+    }
+  };
 }
 
-// listen for app coming back online
-window.addEventListener('online', checkDatabase);
+function saveRecord(record) {
+  console.log("Save record invoked");
+  // Create a transaction on the BudgetStore db with readwrite access
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  // Access your BudgetStore object store
+  const store = transaction.objectStore("BudgetStore");
+
+  // Add record to your store with add method.
+  store.add(record);
+}
+
+// Listen for app coming back online
+window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,80 @@
 console.log('Hi from your service-worker.js file!');
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+const FILES_TO_CACHE = [
+    '/',
+    "/manifest.webmanifest",
+    "/styles.css",
+    '/db.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/index.html',
+    '/index.js',
+    
+
+]
+// install
+self.addEventListener('install', function (evt) {
+    // pre cache image data
+   
+
+    // pre cache all static assets
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+  });
+
+
+  //Activate
+  self.addEventListener('activate', function(evt) {
+      evt.waitUntil(
+          caches.keys().then((keyList) => {
+              return Promise.all(
+                  keyList.map((key) => {
+                      if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                          console.log('Removing old cache data', key);
+                          return caches.delete(key);
+                      }
+                  })
+              )
+          })
+      )
+  })
+ 
+  // fetch 
+
+  self.addEventListener('fetch', function (evt) {
+    // code to handle requests goes here
+    if(evt.request.url.includes('/api/')){
+        console.log('[Service Worker] Fetch (data)', evt.request.url);
+
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response =>{
+                    if(response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            })
+        );
+        return;
+    }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
